0c70067adc94c4e480725f1a5b6ca1b050535e28
arch-yzk <archc18yh27zno3k@gmail.com>
2020-01-09 13:38:08 +0900

    Thread

---
+++ NetworkProgramming/2020_01_09/SimpleThreadTest2.java
@@ -0,0 +1,19 @@
+class MyThread2  implements Runnable {
+	public void run() {
+		for (int i = 0; i < 100; i++) {
+			System.out.println("MyThreadのrunメソッド(" + i + ")");
+		}
+	}
+}
+
+public class SimpleThreadTest2 {
+	public static void main(String[] args) {
+		MyThread2 t = new MyThread2();
+		Thread thread = new Thread(t);
+		thread.start();
+
+		for (int i = 0; i < 100; i++) {
+			System.out.println("SimpleThreadTest2のmainメソッド(" + i + ")");
+		}
+	}
+}
