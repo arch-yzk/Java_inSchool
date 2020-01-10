688aa51ab92d6ff6e2b79bf75e13a4a890c1e018
arch-yzk <archc18yh27zno3k@gmail.com>
2020-01-10 15:39:26 +0900

    SoukoMain.java

--- NetworkProgramming/2020_01_09/SoukoMain.java
+++ NetworkProgramming/2020_01_09/SoukoMain.java
@@ -9,15 +9,14 @@ class Souko
         this.zaiko = n;
     }
 
-    int getZaiko()
+    synchronized int getZaiko()
     {
         return zaiko;
     }
 
     synchronized int pickZaiko(int n)
     {
-        zaiko = zaiko - n;
-        return zaiko;
+        return zaiko -= n;
     }
 }
 
@@ -26,6 +25,7 @@ class Operator extends Thread
     private Souko souko;
     private String name;
     private int pickCount;
+    private int flag = 0;
     private static Object monitor = new Object(); //ロックを確実にする専用オブジェクト
 
     Operator(Souko souko, String name, int pickCount)
@@ -37,28 +37,40 @@ class Operator extends Thread
 
     public void run()
     {
-        while(souko.getZaiko() != 0)
+        for(;;)
         {
             synchronized(monitor)
             {
-                if(souko.getZaiko() < this.pickCount)
+                if(souko.getZaiko() == 0)
                 {
-    
-                    System.out.println(name + "さんが一度に取れる個数以下なので終了");
+                    break;
+                }
+                else if(souko.getZaiko() < pickCount)
+                {
+                    int buffer = souko.getZaiko();
+                    souko.pickZaiko(souko.getZaiko());
+                    System.out.println(this.name + "さんが" + buffer + "個とったので、残り" + souko.getZaiko());
                     break;
                 }
                 souko.pickZaiko(this.pickCount);
-                System.out.println(this.name + "さんが" + pickCount + "とったので、残り" + souko.getZaiko());
+                System.out.println(this.name + "さんが" + this.pickCount + "個とったので、残り" + souko.getZaiko());
             }
-            Random randomGenerator = new Random();
-            int num = randomGenerator.nextInt(500);
-            try
+            Thread.yield();
+            this.flag++;
+            if(this.flag >= 2)
             {
-                sleep(num);
-            }
-            catch(InterruptedException e)
-            {
-                System.out.println(e);
+                try
+                {
+                    sleep(5);
+                }
+                catch(InterruptedException e)
+                {
+                    System.out.println(e);
+                }
+                finally
+                {
+                    this.flag = 0;
+                }
             }
         }
     }
@@ -68,25 +80,26 @@ public class SoukoMain
 {
     public static void main(String[] args)
     {
-        System.out.println("このプログラムはランダムにスレッドを実行したいため、sleep()を使って処理を重くしています。");
+        //System.out.println("このプログラムはランダムにスレッドを実行したいため、sleep()を使って処理を重くしています。");
         Souko souko = new Souko(10);
         Operator A = new Operator(souko, "A", 1);
         Operator B = new Operator(souko, "B", 2);
-
         
         souko.getZaiko();
+        
         A.start();
         B.start();
 
         try
         {
-            A.join();
             B.join();
+            A.join();
         }
         catch(InterruptedException e)
         {
             System.out.println(e);
         }
+        
 
         System.out.println("作業終了");
     }
